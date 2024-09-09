45a1309c3ce1688239a33e25b0cd886354ef3b46
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-09-09 09:31:50 +0200

    dummy lexer

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
-enum NodeType
-{
+use std::vec;
+
+enum NodeType {
     Number(i32),
     Add,
-    Multiply
+    Multiply,
 }
 
 struct Node {
@@ -11,12 +12,19 @@ struct Node {
     right: Option<Box<Node>>,
 }
 
+fn tokenize(input: String) -> Vec<NodeType>
+{
+    let mut tokens: Vec<NodeType> = vec![];
+
+    tokens
+}
 
 fn main() -> eyre::Result<()> {
-    let mut input = String::new();
+    loop {
+        let mut input = String::new();
 
-    std::io::stdin().read_line(&mut input)?;
+        std::io::stdin().read_line(&mut input)?;
 
-    println!("You typed: {}", input.trim());
-    Ok(())
+        println!("You typed: {}", input.trim());
+    }
 }
